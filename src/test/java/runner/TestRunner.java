5f982a3b023345b5a25ca1e197e1a9cbe59e31a0
Monisha <monisha2ashokkumar@gmail.com>
2022-06-19 14:27:11 +0800

    Config management and supporting methods

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,30 @@
 package runner;
 
+import browser.driver.DriverManager;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features", glue = "stepDefinitions", plugin = {"html:target/cucumber-reports"})
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        plugin = {"html:src/reports/cucumber-reports.html", "rerun:src/reports/failedScenarios.txt",
+        "pretty","json:src/reports/cucumber-reports.json"}
+)
 public class TestRunner {
+    // Handling browser sessions on hooks for better control
+    // To open the browser before start of all executions
+    @BeforeClass
+    public static void openBrowserSession() {
+        DriverManager.getBrowser();
+    }
+
+    // To terminate the browser after all executions
+    @AfterClass
+    public static void closeBrowserSession() {
+        DriverManager.closeSession();
+    }
 }
